d33cd3df21ef5182a5c56e84bc10262ba84f67f5
ryu60412 <simon.ryu60412@gmail.com>
2026-05-25 13:01:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'otetsudai-v3';
+const CACHE_NAME = 'otetsudai-v3b';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
